c524a38d519ee4f126827aef54d903b1e99cc590
Biblioteca Alfonso Borrero Cabal SJ <BibliotecaPUJ@JAVERIANA.LOC>
2022-03-31 12:33:16 -0500

    algo se hizo

--- Proyecto.c
+++ Proyecto.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
 
-void main(){
-  print("nada");
+
+enum tags
+{
+    ciencia,
+    tecnologia,
+    politica,
+    deportes,
+    otros
+};
+
+
+
+struct Ssus
+{
+    enum tags *listaTags[5];
+    int consecutivo=0;
+};
+
+void añadirTag(struct Ssus s, enum tags nuevo){
+    s->listaTags[s->consecutivo] = nuevo;
 }
+
+struct Spub
+{
+};
+struct SC
+{
+};
+
+
+void main()
+{
+    struct Ssus suscr;
+
+}
+
+
+
+
