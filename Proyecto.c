3c04459e80b20ecef3a7bb26ec9140867dccac2a
Biblioteca Alfonso Borrero Cabal SJ <BibliotecaPUJ@JAVERIANA.LOC>
2022-03-31 20:35:45 -0500

    peor es nada

--- Proyecto.c
+++ Proyecto.c
@@ -3,32 +3,40 @@
 
 enum tags
 {
-    ciencia,
-    tecnologia,
-    politica,
-    deportes,
-    otros
-};
-
-
-
-struct Ssus
-{
-    enum tags *listaTags[5];
-    int consecutivo=0;
+    Arte,
+    Farandula,
+    Ciencia,
+    Politica,
+    Sucesos,
+    Nada
 };
 
 struct Spub
 {
+    enum tags Categoria = Nada;
+    char *contenido[100];
 };
+
 struct SC
 {
+
 };
+
+struct Ssus
+{
+    struct Spub publiHist[10];
+};
+
+
 
 
 void main()
 {
-    struct Ssus suscr;
+    /// se necesitan 3 pipes
+    int fpubSC[2];/// uno para conectar publicaciones y el SC
+    int fSCsus[2];/// otro para el SC con los sucriptores
+    int fsusSC[2];///y un ultimo que actue como una segunda via que vaya de suscriptores hasta el SC
+
 
 }
 
